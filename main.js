58d7ff1b308fe583c5aae445e48eb8e8c8ec5837
Shaurya Srivastava <srivastavashaurya225@gmail.com>
2024-04-30 11:14:02 +0530

    Javascript Finished

--- main.js
+++ main.js
@@ -1,7 +1,6 @@
 const pick = document.getElementsByTagName("i")
-let playButtons = document.querySelectorAll(".playing_button")
-let playerbuttons = document.querySelectorAll(".playing_button img")
-
+const playButtons = document.querySelectorAll(".playing_button")
+const playerbuttons = document.querySelectorAll(".playing_button img")
 
 let xscore = 0
 let oscore = 0
@@ -34,19 +33,76 @@ function updateScore(){
     document.querySelector(".ties .score").innerText = tiescore
 }
 
+function quit(){
+    xscore = 0
+    oscore = 0
+    tiescore = 0
+    updateScore()
+    player = ""
+    opponent = ""
+    xList = []
+    oList = []
+    document.querySelector(".win_lost_background").style.display="none"
+    document.querySelector(".win_lose").style.display="none"
+    document.querySelector(".starting_menu").style.display = "flex"
+    document.querySelector(".playing").style.display = "none"
+    turn = "x"
+    for(let i = 0;i<playButtons.length;i++){
+        playButtons[i].disabled = false
+        playerbuttons[i].style.display = "none";
+        document.querySelectorAll(".playing_button")[i].style.boxShadow = "0px 8px #111c22";
+    }
+    
+}
+
+function nextRound(){
+    xList = []
+    oList = []
+    document.querySelector(".win_lost_background").style.display="none"
+    document.querySelector(".win_lose").style.display="none"
+    turn = "x"
+    for(let i = 0;i<playButtons.length;i++){
+        playButtons[i].disabled = false
+        playerbuttons[i].style.display = "none";
+        document.querySelectorAll(".playing_button")[i].style.boxShadow = "0px 8px #111c22";
+    }
+}
+
+function whowin(who){
+    document.querySelector(".win_lost_background").style.display="block"
+    document.querySelector(".win_lose").style.display="block"
+    if (who == "x"){
+        document.querySelector(".win_lose p").innerText="PLAYER X WINS"
+        document.querySelector(".whowin img").src = "SVG/icon-x.svg"
+        document.querySelector(".whowin img").style.color = "#31c3bd"
+    }
+    else if(who == "o"){
+        document.querySelector(".win_lose p").innerText="PLAYER O WINS"
+        document.querySelector(".whowin img").src = "SVG/icon-o.svg"
+        document.querySelector(".whowin img").style.color = "#f2b137"
+    }
+    else if(who == "tie"){
+        document.querySelector(".win_lose p").style.display="none"
+        document.querySelector(".whowin img").style.display="none"
+        document.querySelector(".whowin p").innerText="ROUND TIED"
+        document.querySelector(".whowin img").style.color = "#a8bfc9"
+    }
+}
+
 
 function engine(){
     for(let i = 0;i<winSituation.length;i++){
         if(xList.includes(winSituation[i][0]) & xList.includes(winSituation[i][1]) & xList.includes(winSituation[i][2])){
             xscore += 1
-            
+            whowin("x")
         }
         else if(oList.includes(winSituation[i][0]) & oList.includes(winSituation[i][1]) & oList.includes(winSituation[i][2])){
             oscore += 1
-            
+            whowin("o")
         }
         else if(xList.length + oList.length == 9){
             tiescore += 1
+            whowin("tie")
             break
         }
     }
@@ -60,7 +116,7 @@ for(let i = 0;i<pick.length;i++){
                 x.classList.add("active")
                 o.classList.remove("active")
                 document.querySelector(".x_box .scoreTitle").innerText = "X(YOU)"
-                document.querySelector(".o_box .scoreTitle").innerText = "O(CPU)"
+                document.querySelector(".o_box .scoreTitle").innerText = "O(OTHER)"
                 player = "x"
                 opponent = "o"
             } 
@@ -70,13 +126,16 @@ for(let i = 0;i<pick.length;i++){
                 o.classList.add("active")
                 x.classList.remove("active")
                 document.querySelector(".x_box .scoreTitle").innerText = "O(YOU)"
-                document.querySelector(".o_box .scoreTitle").innerText = "X(CPU)"
+                document.querySelector(".o_box .scoreTitle").innerText = "X(OTHER)"
                 player = "o"
                 opponent = "x"
             }
         }
     })
 }
+function getRandomInt(max) {
+    return Math.floor(Math.random() * max);
+  }
 
 
 for(let i = 0;i<playButtons.length;i++){
@@ -100,17 +159,24 @@ for(let i = 0;i<playButtons.length;i++){
     })
 }
 
-
-
-function start(){
-    console.log("player: "+player+" opponent:"+opponent)
+function startplaying(){
     document.querySelector(".starting_menu").style.display = "none"
-    document.querySelector(".playing").style.display = "block"
+    document.querySelector(".playing").style.display = "flex"
 }
 
 function restart(){
-    document.querySelector(".starting_menu").style.display = "flex"
-    document.querySelector(".playing").style.display = "none"
+    xscore = 0
+    oscore = 0
+    tiescore = 0
+    updateScore()
+    xList = []
+    oList = []
+    document.querySelector(".win_lost_background").style.display="none"
+    document.querySelector(".win_lose").style.display="none"
+    turn = "x"
+    for(let i = 0;i<playButtons.length;i++){
+        playButtons[i].disabled = false
+        playerbuttons[i].style.display = "none";
+        document.querySelectorAll(".playing_button")[i].style.boxShadow = "0px 8px #111c22";
+    }
 }
-
-
